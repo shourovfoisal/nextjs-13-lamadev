ed47974917b31ee06a4eea1a0176071c7f3b4319
Shourov Foisal <shourovfoisal@gmail.com>
2023-07-08 15:24:07 +0600

    learned app route, route groups, special pages like loading, error etc.
    also used components and layout page.

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const BlogLayout = ({ children }) => {
+  return children
+}
+
+export default BlogLayout
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>&copy;2023 Lamamia. All rights reserved.</div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
